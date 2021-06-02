92431287ca634e70f28d4900ccd6476254ae12e4
Václav Mejsnar <69161210+mejsnarv@users.noreply.github.com>
2021-06-02 17:09:14 +0200

    Add files via upload

---
+++ delete_modern_apps.ps1
@@ -0,0 +1,2 @@
+﻿Get-AppxPackage | Remove-AppxPackage
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.WindowsStore_11909.1002.3.0_x64__8wekyb3d8bbwe\appxmanifest.xml" -DisableDevelopmentMode
